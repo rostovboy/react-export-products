3eb473fdb492c8066a11e5abb4776adec62a4199
Alexey T <rostovboy@gmail.com>
2023-01-27 10:07:21 +0300

    Beta

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import CheckboxTree from 'react-checkbox-tree'
 import 'react-checkbox-tree/lib/react-checkbox-tree.css'
 import { Node } from 'react-checkbox-tree'
+import axios from 'axios'
 //import { nodes } from './data/nodes1'
 
 library.add(faCheckSquare, faSquare, faChevronRight, faChevronDown, faPlusSquare, faMinusSquare, faFile, faFolderOpen, faFolder)
@@ -22,8 +23,14 @@ library.add(faCheckSquare, faSquare, faChevronRight, faChevronDown, faPlusSquare
 interface IState {
     checked: [],
     expanded: [],
-    nodes: Node[];
+    nodes: Node[],
+    result: string,
+    isTreeLoading: boolean,
+    isLinkLoading: boolean
 }
+
+const apiEndpoint = 'https://cors-anywhere.herokuapp.com/https://grandfayans.ru/api/categories'
+const authorizationToken = 'Token 67c5604a-0f50-4ad1-927e-ca70200b03d0'
 
 class App extends React.Component {
 
@@ -31,9 +38,14 @@ class App extends React.Component {
         checked: [],
         expanded: [],
         nodes: [],
-    };
+        result: '',
+        isTreeLoading: true,
+        isLinkLoading: true
+    }
 
-    async componentDidMount() {
+
+
+    /* async componentDidMount() {
         try {
             const response = await fetch('https://grandfayans.ru/api/categories', {
                 headers: {
@@ -41,40 +53,132 @@ class App extends React.Component {
                 },
                 //mode: 'no-cors',
             });
-            const data = await response.json();
-            this.setState({ nodes: data.results });
+            const data = await response.json()
+            this.setState({ nodes: data.results })
+        } catch (error) {
+            // Handle the error
+        }
+    } 
+    
+    handleSubmit = () => {
+        const { checked } = this.state;
+        fetch('https://cors-anywhere.herokuapp.com/https://grandfayans.ru/api/categories', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Token 67c5604a-0f50-4ad1-927e-ca70200b03d0',
+            },
+            body: JSON.stringify({ checked }),
+        })
+    } */
+
+    handleSelectAll = () => {
+        const { nodes } = this.state
+        console.log(nodes)
+        const allNodeIds = nodes.map(node => node.value)
+        console.log(allNodeIds)
+        this.setState({ checked: allNodeIds })
+    }
+
+    async componentDidMount() {
+        try {
+            const response = await axios.get(apiEndpoint, {
+                headers: {
+                    'Authorization': authorizationToken,
+                },
+            })
+            this.setState({ nodes: response.data.results, isTreeLoading: false })
         } catch (error) {
             // Handle the error
         }
     }
 
+    handleSubmit = async () => {
+        const { checked } = this.state
+        try {
+            const response = await axios.post(apiEndpoint, { checked }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': authorizationToken,
+                },
+            })
+            this.setState({ result: response.data.object, isLinkLoading: false })
+        } catch (error) {
+            // Handle the error
+        }
+    }
+
+    clearChecked = () => {
+        this.setState({ checked: [], result: '' });
+    }
+
     render() {
-        const { nodes } = this.state
+        const { nodes, result, checked, isTreeLoading, isLinkLoading } = this.state;
 
         console.log(this.state)
 
         return (
-            <CheckboxTree
-                nodes={nodes}
-                checked={this.state.checked}
-                expanded={this.state.expanded}
-                onCheck={checked => this.setState({ checked })}
-                onExpand={expanded => this.setState({ expanded })}
-                showExpandAll={true}
-                iconsClass="fa5"
-                icons={{
-                    check: <FontAwesomeIcon className="rct-icon rct-icon-check" icon={['far', 'check-square']} />,
-                    uncheck: <FontAwesomeIcon className="rct-icon rct-icon-uncheck" icon={['far', 'square']} />,
-                    halfCheck: <FontAwesomeIcon className="rct-icon rct-icon-half-check" icon={['far', 'check-square']} />,
-                    expandClose: <FontAwesomeIcon className="rct-icon rct-icon-expand-close" icon="chevron-right" />,
-                    expandOpen: <FontAwesomeIcon className="rct-icon rct-icon-expand-open" icon="chevron-down" />,
-                    expandAll: <FontAwesomeIcon className="rct-icon rct-icon-expand-all" icon={['far', 'square-plus']} />,
-                    collapseAll: <FontAwesomeIcon className="rct-icon rct-icon-collapse-all" icon={['far', 'square-minus']} />,
-                    parentClose: <FontAwesomeIcon className="rct-icon rct-icon-parent-close" icon={['far', 'folder']} />,
-                    parentOpen: <FontAwesomeIcon className="rct-icon rct-icon-parent-open" icon={['far', 'folder-open']} />,
-                    leaf: <FontAwesomeIcon className="rct-icon rct-icon-leaf-close" icon={['far', 'file']} />
-                }}
-            />
+            <div>
+                {isTreeLoading ? (
+                    <div className="spinner-border text-primary" role="status">
+                        <span className="sr-only">Загружается структура каталога...</span>
+                    </div>
+                ) : (
+                    <div>
+                        <CheckboxTree
+                            nodes={nodes}
+                            checked={this.state.checked}
+                            expanded={this.state.expanded}
+                            onCheck={checked => this.setState({ checked })}
+                            onExpand={expanded => this.setState({ expanded })}
+                            showExpandAll={true}
+                            iconsClass="fa5"
+                            icons={{
+                                check: <FontAwesomeIcon className="rct-icon rct-icon-check" icon={['far', 'check-square']} />,
+                                uncheck: <FontAwesomeIcon className="rct-icon rct-icon-uncheck" icon={['far', 'square']} />,
+                                halfCheck: <FontAwesomeIcon className="rct-icon rct-icon-half-check" icon={['far', 'check-square']} />,
+                                expandClose: <FontAwesomeIcon className="rct-icon rct-icon-expand-close" icon="chevron-right" />,
+                                expandOpen: <FontAwesomeIcon className="rct-icon rct-icon-expand-open" icon="chevron-down" />,
+                                expandAll: <FontAwesomeIcon className="rct-icon rct-icon-expand-all" icon={['far', 'square-plus']} />,
+                                collapseAll: <FontAwesomeIcon className="rct-icon rct-icon-collapse-all" icon={['far', 'square-minus']} />,
+                                parentClose: <FontAwesomeIcon className="rct-icon rct-icon-parent-close" icon={['far', 'folder']} />,
+                                parentOpen: <FontAwesomeIcon className="rct-icon rct-icon-parent-open" icon={['far', 'folder-open']} />,
+                                leaf: <FontAwesomeIcon className="rct-icon rct-icon-leaf-close" icon={['far', 'file']} />
+                            }}
+                        />
+
+                        <div>
+                            <button className="btn btn-primary mt-5 mb-5" onClick={this.handleSelectAll}>Выбрать все</button>
+                        </div>
+
+                        {result ? (
+                            <div>
+                                {isLinkLoading ? (
+                                    <div>
+                                        <button className="btn btn-primary mt-5 mb-5" type="button" disabled>
+                                            <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                                            <span className="sr-only">Экспорт...</span>
+                                        </button>
+                                    </div>
+                                ) : (
+                                    <div>
+                                        <a className="btn btn-primary mt-5 mb-5" href={result} onClick={this.clearChecked}>Скачать архив</a>
+                                    </div>
+                                )}
+                            </div>
+                        ) : (
+                            <button
+                                className="btn btn-primary mt-5 mb-5"
+                                disabled={checked.length === 0}
+                                onClick={this.handleSubmit}
+                            >
+                                Экспорт
+                            </button>
+                        )}
+
+                    </div>
+                )}
+            </div>
         )
     }
 }
